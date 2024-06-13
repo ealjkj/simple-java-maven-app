c0b75a38473f46292b28a66b2b58260fc89a5a25
Jorge <ealjkj@gmail.com>
2024-06-13 15:31:51 -0700

    adds permisions

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,4 +24,5 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 ls target
+chmod +r target/my-app-1.0-SNAPSHOT.jar
 java -jar target/${NAME}-${VERSION}.jar
